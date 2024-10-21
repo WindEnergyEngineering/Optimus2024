6e215e71dbdee3225e93b4fceb4b7ed54d55f231
brudoehlo <98698631+brudoehlo@users.noreply.github.com>
2024-10-21 17:37:03 +0200

    DLC 6.1 Preperation

--- IEA-3.4-130-RWT/openfast/result_print.m
+++ IEA-3.4-130-RWT/openfast/result_print.m
@@ -23,15 +23,14 @@ end
 
 %% PostProcessing FAST
 fid         = fopen(OutputFile);
-formatSpec  = repmat('%f',1,10);
+formatSpec  = repmat('%f',1,39);
 FASTResults = textscan(fid,formatSpec,'HeaderLines',8);
 Time        = FASTResults{:,1};
 Wind1VelX   = FASTResults{:,2};
-RotSpeed    = FASTResults{:,4};
-TTDspFA     = FASTResults{:,5};
-GenPwr      = FASTResults{:,9};
+BldPitch1   = FASTResults{:,5};
+RotSpeed    = FASTResults{:,7};
+Azimuth     = FASTResults{:,6};
 fclose(fid);
-
 
 figure
 
@@ -48,15 +47,57 @@ hold on;box on;grid on;
 plot(Time,RotSpeed)
 ylabel('rotor speed [rpm]')
 
-% generator power
+% BldPitch1
 subplot(413)
 hold on;box on;grid on;
-plot(Time,GenPwr)
-ylabel('generator power [kW]')
+plot(Time,BldPitch1)
+ylabel('Blade 1 Pitch [°]')
 
-% plot tower top position
+% plot Azimuth
 subplot(414)
 hold on;box on;grid on;
-plot(Time,TTDspFA)
+plot(Time,Azimuth)
 xlabel('time [s]')
-ylabel('tower top position [m]')
+ylabel('Azimuth [°]')
+%%
+saveas(gcf,'Barchart.png')
+
+
+%%
+
+RotTorq   = FASTResults{:,8};
+RootFxb1   = FASTResults{:,14};
+RootFyb1    = FASTResults{:,15};
+RootFzb1     = FASTResults{:,16};
+RootMxb1   = FASTResults{:,17};
+RootMyb1   = FASTResults{:,18};
+RootMzb1    = FASTResults{:,19};
+RootFxc1     = FASTResults{:,20};
+RootFyc1   = FASTResults{:,21};
+RootMxc1   = FASTResults{:,22};
+RootMyc1    = FASTResults{:,23};
+LSShftFxa     = FASTResults{:,24};
+LSShftFya   = FASTResults{:,25};
+LSShftFza   = FASTResults{:,26};
+LSShftMxa    = FASTResults{:,27};
+LSSTipMya     = FASTResults{:,28};
+LSSTipMza   = FASTResults{:,29};
+LSShftFys   = FASTResults{:,30};
+LSShftFzs    = FASTResults{:,31};
+LSSTipMys     = FASTResults{:,32};
+LSSTipMzs   = FASTResults{:,33};
+YawBrFxp   = FASTResults{:,34};
+YawBrFyp    = FASTResults{:,35};
+YawBrFzp     = FASTResults{:,36};
+YawBrMxp  = FASTResults{:,37};
+YawBrMyp   = FASTResults{:,38};
+YawBrMzp    = FASTResults{:,39};
+
+max_Root = [max(RootFxb1)  max(RootFyb1) max(RootFzb1) max(RootMxb1) max(RootMyb1) max(RootMzb1) max(RootFxc1) max(RootFyc1) max(RootMxc1) max(RootMyc1)];
+max_LSS = [max(LSShftFxa) max(LSShftFya) max(LSShftFza) max(LSShftMxa) max(LSSTipMya) max(LSSTipMza) max(LSShftFys) max(LSShftFzs) max(LSSTipMys) max(LSSTipMzs)];
+max_Yaw = [max(YawBrFxp) max(YawBrFyp) max(YawBrFzp) max(YawBrMxp) max(YawBrMyp) max(YawBrMzp)];
+output = [max_Root max_LSS max_Yaw]
+csvwrite('Output.txt',output)
+%find(RootFxb1 == max_Root(:,1))
+%find(RootFyb1 == max_Root(:,2))
+%line = [find(RootFxb1 == max_Root(:,1)) max(RootFyb1) max(RootFzb1) max(RootMxb1) max(RootMyb1) max(RootMzb1) max(RootFxc1) max(RootFyc1) max(RootMxc1) max(RootMyc1)]
