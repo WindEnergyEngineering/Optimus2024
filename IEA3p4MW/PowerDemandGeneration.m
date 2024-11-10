3ef1175f40e32bca99c1e788168b1e80617e9efb
Plutoniumdrache <76872934+Plutoniumdrache@users.noreply.github.com>
2024-11-10 15:21:29 +0100

    power demand curve created

--- IEA3p4MW/PowerDemandGeneration.m
+++ IEA3p4MW/PowerDemandGeneration.m
@@ -7,40 +7,31 @@ curtailment_duration = 4*3600; % [s]
 
 dt = 0.1; % [s]
 Tmax = 3600*25; % [s]
+t = 0:0.1:Tmax-dt; % [s]
 
-t = 0:0.1:Tmax-dt;
+p_begin_i   = 3600*11; % [s]
+p_end_i     = 3600*15; % [s]
+delta = abs(p_end_i - p_begin_i); % [s]
+curtailment_time = p_begin_i:dt:p_end_i; % [s]
 
-% p_begin_i   = 3600*11;
-% p_end_i     = 3600*15;
-% delta = abs(p_end_i - p_begin_i);
-% curtailment_time = -delta/2:0.1:delta/2;
-
-% parabel =(curtailment_time).^2 - max((curtailment_time).^2);
-% figure;
-% plot(curtailment_time,parabel)
-
-% values1 = P_dem * ones(1,(p_begin_i)/dt);
-% values2 = P_dem * ones(1,length(t)-(length(values1)+length(curtailment_time)));
-% v = [values1, parabel, values2];
-% figure;
-% plot(t,v);
 
 syms a b c
-eqn1 = a*(39600)^2 + b*39600 + c == 3.4e6;
-eqn2 = a*(46800)^2 + b*46800 + c == 3.4e6*0.94;
-eqn3 = a*(54000)^2 + b*54000 + c == 3.4e6;
+eqn1 = a*(p_begin_i)^2 + b*p_begin_i + c == 1;
+eqn2 = a*(p_begin_i+delta/2)^2 + b*(p_begin_i+delta/2) + c == (1-curtailment_rate);
+eqn3 = a*(p_end_i)^2 + b*p_end_i + c == 1;
 
 [A,B] = equationsToMatrix([eqn1,eqn2,eqn3],[a,b,c]);
-X = linsolve(A,B)
+X = linsolve(A,B);
 
-p_begin_i   = 3600*11;
-p_end_i     = 3600*15;
-delta = abs(p_end_i - p_begin_i);
-curtailment_time = p_begin_i:dt:p_end_i;
-
-c_square = curtailment_time.^2;
-
-parabel = X(1)*c_square + X(2)*curtailment_time + X(3);
+parabel = double(X(1))*curtailment_time.^2 + double(X(2))*curtailment_time + double(X(3));
 figure;
 plot(parabel)
 
+values1 = P_dem * ones(1,(p_begin_i)/dt);
+values2 = P_dem * ones(1,length(t)-(length(values1)+length(curtailment_time)));
+Disturbance.p_0.signal.values = [values1, parabel*P_dem, values2];
+Disturbance.p_0.time = t;
+figure;
+plot(t,Disturbance.p_0.signal.values);
+
+save(['power\PDem_0d06curtailment','_Disturbance'],'Disturbance')
