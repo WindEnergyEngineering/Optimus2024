cd84259831dda49813c9b984171d7dccbd1db32f
Plutoniumdrache <76872934+Plutoniumdrache@users.noreply.github.com>
2024-11-18 22:37:09 +0100

    faster wind speed changes

--- OptimusShakti5MW/StaticCalculationsConfig.m
+++ OptimusShakti5MW/StaticCalculationsConfig.m
@@ -66,7 +66,7 @@ switch CalculationName
         
     case {'NREL5MW_FBSWE'}        
         % Exercise 8.2a: adjusted by fle (12.11.24)
-  
+        
         % Default
         Parameter                       	= DefaultParameter_SLOW2DOF;
         Parameter                           = DefaultParameter_FBv1_ADv14(Parameter);           
@@ -79,14 +79,18 @@ switch CalculationName
         % Wind speeds
         v_0         = 3.5:.1:30; % [m/s]
         
+        % Region wind speeds produced by HowToFindRegionWindSpeed.m
+        % legend  = ["rated"    "1"     "1.5"       "2.5"];
+        v_regions = [9.5775    2.7052    6.3873    9.3838]; % [m/s]
+
         % find v_rated
         v_0_min                         = 0;
         v_0_max                         = 30;        
         Omega                           = Parameter.CPC.Omega_g_rated/Parameter.Turbine.r_GB;
         theta                           = Parameter.CPC.theta_min;  
         M_g                             = Parameter.VSC.M_g_rated;
-        Parameter.VSC.v_rated         	= 9.5778;  % the calculation are done in the function 'HowToFindRatedWindSpeed' 
-                                                    % the value from section %%Optimization using fminuncis used
+        Parameter.VSC.v_rated         	= v_regions(1); 
+
         % find v_1
         v_0_min                         = 0;
         v_0_max                         = 30;
@@ -94,8 +98,8 @@ switch CalculationName
         k                               = Parameter.VSC.k;  
         Omega                           = Parameter.VSC.Omega_g_1d5/Parameter.Turbine.r_GB;
         M_g                             = 0;
-        Parameter.VSC.v_1               = 3.0;      % the calculation are done in the function 'HowToFindRegionWindSpeed' 
-                                                    % the value from section %%Optimization using fminbnd used
+        Parameter.VSC.v_1               = v_regions(2); % Davids value was 3.0 m/s here      
+                                                    
         % find v_1d5
         v_0_min                         = 0;
         v_0_max                         = 30;
@@ -103,8 +107,8 @@ switch CalculationName
         k                               = Parameter.VSC.k;  
         Omega                           = Parameter.VSC.Omega_g_1d5/Parameter.Turbine.r_GB;
         M_g                             = k*(Omega*Parameter.Turbine.r_GB)^2;
-        Parameter.VSC.v_1d5             = 6.3873;   % the calculation are done in the function 'HowToFindRegionWindSpeed' 
-                                                    % the value from section %%Optimization using fminbnd used
+        Parameter.VSC.v_1d5             = v_regions(3);  
+                                                   
 
         % find v_2d5
         v_0_min                         = 0;
@@ -113,9 +117,7 @@ switch CalculationName
         k                               = Parameter.VSC.k;  
         Omega                           = Parameter.CPC.Omega_g_rated/Parameter.Turbine.r_GB;
         M_g                             = k*(Omega*Parameter.Turbine.r_GB)^2;
-        Parameter.VSC.v_2d5             = 9.3838;   % the calculation are done in the function 'HowToFindRegionWindSpeed' 
-                                                    % the value from section %%Optimization using fminbnd used
-
+        Parameter.VSC.v_2d5             = v_regions(4);
 end
 
 end
