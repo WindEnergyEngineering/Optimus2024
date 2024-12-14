30a64ca727cffc3cb5cd7266bb2500378fa4d0a6
Plutoniumdrache <76872934+Plutoniumdrache@users.noreply.github.com>
2024-12-14 16:55:09 +0100

    bending stiffness tower

--- IEA-3.4-130-RWT/Optimus_HH140_D178/DLC_1_2/CalcBendingStiffness.m
+++ IEA-3.4-130-RWT/Optimus_HH140_D178/DLC_1_2/CalcBendingStiffness.m
@@ -14,9 +14,11 @@
 % - code based on: SummerGames2024 IEA Task52
 
 clearvars;close all;
+
 %% select wind speed range
-wind = 3:0.5:6; % wind speed range
+wind = 3:0.1:9; % wind speed range
 %% Generate FAST simulation outputs
+tic
 for i = 1:length(wind)
     
     % find parameter placeholder in inflow file and replace with wind speed
@@ -34,10 +36,11 @@ for i = 1:length(wind)
     
     % rename output and move file to output directory
     OutputFile  = 'IEA-3.4-130-RWT.out';
+     
     new_filename = "OutputFiles\" + "IEA-3.4-130-RWT" + num2str(wind(i),'%02.1f') + ".out";
     movefile(OutputFile,new_filename);
 end
-
+toc
 %% PostProcessing FAST
 V_test = wind;
 
@@ -55,16 +58,16 @@ for idx = 1:length(V_test)
 %     [Channels, ChanName, ChanUnit, FileID, DescStr] =  ReadFASTbinary(OutputFileFAST);
 
     fid         = fopen(OutputFileFAST);
-    formatSpec  = repmat('%f',1,13);
+    formatSpec  = repmat('%f',1,16);
     FASTResults = textscan(fid,formatSpec,'HeaderLines',8);
     Time        = FASTResults{:,1};
     Wind1VelX   = FASTResults{:,2};
-    RotSpeed    = FASTResults{:,4};
-    TTDspFA     = FASTResults{:,5};
-    TwrTpTDxi   = FASTResults{:,9};
-    YawBrTDxp   = FASTResults{:,10};
-    RtAeroFxi   = FASTResults{:,11};
-    GenPwr      = FASTResults{:,12};
+    RotSpeed    = FASTResults{:,6};
+    TTDspFA     = FASTResults{:,7};
+    TwrTpTDxi   = FASTResults{:,12};
+    YawBrTDxp   = FASTResults{:,13};
+    LSShftFxa   = FASTResults{:,14};
+    GenPwr      = FASTResults{:,15};
     fclose(fid);
 
     % last 3 revolutions
@@ -84,67 +87,48 @@ for idx = 1:length(V_test)
     StadyStatesFAST.x_t(idx)    = mean(TTDspFA(index_t:end));      % [m]
     StadyStatesFAST.x_ti(idx)   = mean(TwrTpTDxi(index_t:end));      % [m]
     StadyStatesFAST.x_tp(idx)   = mean(YawBrTDxp(index_t:end));      % [m]
-
     % Generator torque 
 %     StadyStatesFAST.M_g(idx) = mean(GenTq(index_t:end));         % [kNm]
     % Aerodynamic force
-    StadyStatesFAST.F_a(idx) = mean(RtAeroFxi(index_t:end));     % [Nm]
-
+%     StadyStatesFAST.F_a(idx) = mean(RtAeroFxi(index_t:end));         % [N] only in aerodyn v15 supported  
+    StadyStatesFAST.LSShftFxa(idx)   = mean(LSShftFxa(index_t:end)*1000); % [N] % this one leads to the correct initial tower top deflection
+    % if you want to know why you need to dig into the aerodyn module...
 end
 %% Plot FAST Steady States
-% figure('Name','Omega')
-% hold on;grid on;box on;
-% % plot(v_0,radPs2rpm(Omega),'.')
-% plot(StadyStatesFAST.v_0,StadyStatesFAST.Omega,'.')
-% xlabel('v_0 [m/s]')
-% ylabel('\Omega [rpm]')
-% legend('SLOW','FAST','Location','southeast')
-
-% figure('Name','theta')
-% hold on;grid on;box on;
-% plot(v_0,rad2deg(theta),'.')
-% plot(StadyStatesFAST.v_0,StadyStatesFAST.theta,'.')
-% xlabel('v_0 [m/s]')
-% ylabel('\theta [deg]')
-% legend('SLOW','FAST','Location','southeast')
-
-% figure('Name','M_g')
-% hold on;grid on;box on;
-% % plot(v_0,M_g,'.')
-% plot(StadyStatesFAST.v_0,StadyStatesFAST.M_g*1e3,'.')
-% xlabel('v_0 [m/s]')
-% ylabel('M_g [Nm]')
-% legend('SLOW','FAST','Location','southeast')
-
 figure('Name','x_T')
 hold on;grid on;box on;
 % plot(v_0,x_T,'.')
 plot(StadyStatesFAST.v_0,StadyStatesFAST.x_t,'.')
 xlabel('v_0 [m/s]')
 ylabel('x_T [m]')
-legend('FAST','Location','southeast')
-
-% figure('Name','Torque Controller')
-% hold on;grid on;box on;
-% plot(radPs2rpm(Omega),M_g/1e3,'.-')
-% plot(StadyStatesFAST.Omega,StadyStatesFAST.M_g,'.-')
-% xlabel('Omega [rpm]')
-% ylabel('M_g [kNm]')
 % legend('SLOW','FAST','Location','southeast')
+
+figure('Name','F_a')
+hold on;grid on;box on;
+% plot(StadyStatesFAST.v_0,StadyStatesFAST.F_a,'.','Color','b') % only in aerodyn v15 supported
+plot(StadyStatesFAST.v_0,StadyStatesFAST.LSShftFxa,'.','Color','r')
+xlabel('Time [s]')
+ylabel('F_a [N]')
+% legend('Aerodyn15','Elastodyn','Location','southeast') % only in aerodyn v15 supported
 
 cut = 0;
 x_t = StadyStatesFAST.x_t(1:end-cut);
 x_ti = StadyStatesFAST.x_ti(1:end-cut);
 x_tp = StadyStatesFAST.x_tp(1:end-cut);
-F_a = StadyStatesFAST.F_a(1:end-cut);
+% F_a = StadyStatesFAST.F_a(1:end-cut); % only in aerodyn v15 supported
+F_a_elastodyn = StadyStatesFAST.LSShftFxa(1:end-cut);
+
 figure('Name','tower equvivalent bending stiffness')
 hold on;box on;grid on;
-plot(x_t,F_a)
-plot(x_ti,F_a)
-plot(x_tp,F_a)
+plot(x_t,F_a_elastodyn)
+plot(x_ti,F_a_elastodyn)
+plot(x_tp,F_a_elastodyn)
 xlabel('TTDspFA tower top position [m]')
 ylabel('RtAeroFxi [N]')
 legend('TTDspFA','TwrTpTDxi','YawBrTDxp')
 
-k_t = F_a./(x_t)
-mean(k_t)
+format compact;
+% inital tower top deflection from basic fit of F_a over x_t for zero force
+x_T0 = -0.021 % [m]
+k_Te = F_a_elastodyn./(x_t-x_T0);
+k_Te = mean(k_Te)
