30cce05fb8e9852a45813a8ef7b2539bc5e4b261
Felix <felix.lehmann@stud.hs-flensburg.de>
2024-12-24 12:45:25 +0100

    4 deg/s pitch speed

--- Simulations/Simulink/DefaultParameter_PitchActuator.m
+++ Simulations/Simulink/DefaultParameter_PitchActuator.m
@@ -3,7 +3,7 @@ function [Parameter] = DefaultParameter_PitchActuator(Parameter)
 % Values from Pietro
 Parameter.PitchActuator.omega           = pi;               % [rad/s]   % frequency
 Parameter.PitchActuator.xi              = 0.8;              % [-]       % damping    
-Parameter.PitchActuator.theta_dot_max 	= deg2rad(2);       % [rad/s]   % max/min pitch rate
+Parameter.PitchActuator.theta_dot_max 	= deg2rad(4);       % [rad/s]   % max/min pitch rate
 Parameter.PitchActuator.theta_max     	= deg2rad(90);      % [rad]     % max pitch angle
 Parameter.PitchActuator.theta_min     	= deg2rad(0);       % [rad]     % min pitch angle
 end
