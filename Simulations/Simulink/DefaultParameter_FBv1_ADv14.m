a96ab4770d19590faefbe39ef4835792ac27449d
Felix <felix.lehmann@stud.hs-flensburg.de>
2024-12-27 13:38:41 +0100

    Add value comments

--- Simulations/Simulink/DefaultParameter_FBv1_ADv14.m
+++ Simulations/Simulink/DefaultParameter_FBv1_ADv14.m
@@ -3,7 +3,7 @@ function [Parameter] = DefaultParameter_FBv1_ADv14(Parameter)
 
 %% Pitch Controller
 Parameter.CPC.theta_K                   = deg2rad(20);                      % [rad]     % gain scheduling parameter, brute-force-optimized
-Parameter.CPC.kp                        = 0.025;                          	% [s]       % proportional gain, brute-force-optimized (adjusted shakti 30.11 educated guess, JP)
+Parameter.CPC.kp                        = 0.025;                          	% [s]       % proportional gain, brute-force-optimized (adjusted shakti 30.11 educated guess, JP),(0.03 works also, a bit mor agrassive fle)
 Parameter.CPC.Ti                        = 10;                               % [s]       % integral gain, brute-force-optimized
 
 Parameter.CPC.Omega_g_rated             = rpm2radPs(428.5);                 % [rad/s]   % rated generator speed, from Shakti - Geno team 24/11/24 fle
@@ -15,7 +15,7 @@ Parameter.CPC.PS                        = [0 Parameter.CPC.theta_min;Parameter.C
 Parameter.VSC.kp                        = 27928.661741;                       % [Nm/(rad/s)]  % proportional gain, first guess (adjusted shakti 12.12 task design script, fle)
 Parameter.VSC.Ti                        = 2.196201;                           % [s]           % integral gain, first guess (adjusted shakti 12.12 task design script, fle)
 % Parameter.VSC.k                         = 1.9;                              % [Nm/(rad/s)^2]% gain region 2, brute-force-optimized
-Parameter.VSC.k                         = .5 * Parameter.General.rho * pi ... % [Nm/(rad/s)^2]% gain region 2, first guess: cp 0.48, lambda 8.75
+Parameter.VSC.k                         = .5 * Parameter.General.rho * pi ... % [Nm/(rad/s)^2]% gain region 2, first guess: cp 0.48, lambda 8.75; 42.9 [NM/(rad/s)^2] brute-force-optimiert (fle)
                                           * Parameter.Turbine.R^5 * .48 / ...
                                           (8.75^3 * Parameter.Turbine.r_GB^3);
 
@@ -27,7 +27,7 @@ Parameter.VSC.Omega_g_1d5               = rpm2radPs(300);                   % [r
 
 Parameter.VSC.Delta_Omega_g             = 0.10*Parameter.CPC.Omega_g_rated; % [rad/s]   % over-/under-speed limit for setpoint-fading, first guess
 Parameter.VSC.Delta_theta               = deg2rad(20);                      % [rad]     % change of pitch angle at which under-speed limit should be reached, brute-force-optimized
-Parameter.VSC.Delta_P                   = 4.95e6;                          	% [W]       % change of power at which over-speed limit should be reached, first guess
+Parameter.VSC.Delta_P                   = 4.95e6;                          	% [W]       % change of power at which over-speed limit should be reached, 4.9 MW brute-force-optimiert (fle)
 
 %% Filter Generator Speed
 Parameter.Filter.LowPass.Enable         = 1;                                % [0/1]     % flag to enable low pass filter for generator speed
